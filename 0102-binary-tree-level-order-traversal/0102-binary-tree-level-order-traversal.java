4c3333bfc30fc5c67975528be67d71e895de35b0
Kunal Pravin Patil <kunalpatil3435@gmail.com>
2026-08-17 12:11:52 +0530

    Time: 17 ms (1.62%), Space: 46.8 MB (62.46%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -13,51 +13,83 @@
  *     }
  * }
  */
+
+ class Pair {
+    TreeNode node;
+    int level;
+    Pair(TreeNode node, int level) {
+        this.node=node;
+        this.level=level;
+    }
+    }
 class Solution {
 
-    private static int levels(TreeNode root) {
+private int levels(TreeNode root) {
         if(root == null) {
             return 0;
         }
 int left = levels(root.left);
 int right= levels(root.right);
         return 1+ Math.max(left ,right);
-    }
-    private static List<Integer> level(TreeNode root,int n, int level,List<Integer> ans ) {
-        if(root== null) {
-            return ans;
+   }
+
+
+    private void level(TreeNode root,List<List<Integer>> res) {
+        
+        Queue<Pair> p = new LinkedList<>();
+
+        if(root!=null) {
+            p.add(new Pair(root,0));
         }
+        while(p.size()>0) {
+        Pair pr= p.remove();
+        TreeNode n= pr.node;
+        int lvl= pr.level;
 
-        if(level==n) {
-            ans.add(root.val);
-            return ans;
+     res.get(lvl).add(n.val);
+        // System.out.print(n.val+ " ");
+        if(n.left!=null) {
+            p.add(new Pair(n.left, lvl+1));
         }
-
-        level(root.left,n,level+1,ans);
-        level(root.right,n,level+1,ans);
-
-      return ans;
-
-
-
-
-
-
+        if(n.right!=null) {
+            p.add(new Pair(n.right, lvl+1));
+        }
     }
+    }
+
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> res= new ArrayList<>();
-        List<Integer> ans= new ArrayList<>();
+        
   
         for(int x=0; x<levels(root);x++) {
-            int n=x;
-            
-            level(root,n,0,ans= new ArrayList<>());
-            res.add(ans);
+            List<Integer> ans= new ArrayList<>();
+         res.add(ans);
         }
-
+        level(root ,res);
         return res;
 
 
         
     }
 }
+
+
+//     private static List<Integer> level(TreeNode root,int n, int level,List<Integer> ans ) {
+//         if(root== null) {
+//             return ans;
+//         }
+
+//         if(level==n) {
+//             ans.add(root.val);
+//             return ans;
+//         }
+
+//         level(root.left,n,level+1,ans);
+//         level(root.right,n,level+1,ans);
+
+//       return ans;
+//     }
+    
+    
+    
+    
